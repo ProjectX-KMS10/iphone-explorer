3ccfb733cbc2015dc0a41764176314143a88d995
清水道浩 <shimizumichihiro@shimizumichihironoMacBook-Pro.local>
2022-08-16 10:27:11 +0900

    footer

--- src/Route/MainLayout.tsx
+++ src/Route/MainLayout.tsx
@@ -6,12 +6,12 @@ import { PageNotFound } from "Pages/PageNotFound";
 
 export const MainLayout = () => {
   return (
-      <Routes>
-        <Route index element={<Navigate to="/signin" />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="topPage" element={<TopPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+    <Routes>
+      <Route index element={<Navigate to="/toppage" />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="toppage" element={<TopPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 };
