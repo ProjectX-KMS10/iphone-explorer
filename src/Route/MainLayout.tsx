23a542fb10dffe68ec9d1743ee029080c98fd430
busiryu <busiryu@busiryunoMacBook-Air.local>
2022-08-23 10:10:49 +0900

    dynamic route

--- src/Route/MainLayout.tsx
+++ src/Route/MainLayout.tsx
@@ -3,11 +3,13 @@ import { SignIn } from "Pages/SignIn";
 import { SignUp } from "Pages/SIgnUp";
 import { TopPage } from "Pages/TopPage";
 import { PageNotFound } from "Pages/PageNotFound";
+import { Article } from "Pages/Article";
 
 export const MainLayout = () => {
   return (
     <Routes>
       <Route index element={<Navigate to="/toppage" />} />
+      <Route path="toppage/:articleId" element={<Article />} />
       <Route path="signin" element={<SignIn />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="toppage" element={<TopPage />} />
