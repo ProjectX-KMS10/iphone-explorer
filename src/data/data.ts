b81d09001c9224b460177f32ba3a84d535d4d99f
busiryu <busiryu@busiryunoMacBook-Air.local>
2022-08-25 10:02:51 +0900

    detail page

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,17 +2,20 @@ export const listData = [
   {
     id: 1,
     name: "iphoneユーザ７割が知らない長押し術",
-    url: "https://source.unsplash.com/reNVIeIHdo8"
+    url: "https://source.unsplash.com/reNVIeIHdo8",
+    title: "test"
   },
   {
     id: 2,
     name: "iphoneユーザ７割が知らない長押し術",
-    url: "https://source.unsplash.com/kARpL0jw9Ak"
+    url: "https://source.unsplash.com/kARpL0jw9Ak",
+    title: "test1"
   },
   {
     id: 3,
     name: "iphoneユーザ７割が知らない長押し術",
-    url: "https://source.unsplash.com/BkS9G1hw70A"
+    url: "https://source.unsplash.com/BkS9G1hw70A",
+    title: "test2"
   },
   {
     id: 4,
